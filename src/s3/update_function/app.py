018edf66bc4ec975a42ceed9f54e4eeec1a2289e
GyeongSu Gim <recona97@gmail.com>
2024-10-22 11:48:02 +0900

    Change directories' name in 'src/s3'

--- src/s3/update_function/app.py
+++ src/s3/update_function/app.py
@@ -3,7 +3,6 @@ import json
 import boto3
 
 
-
 def lambda_handler(event, context):
     if not ("httpMethod" in event and event["httpMethod"] == "GET"):
         return {
@@ -15,10 +14,11 @@ def lambda_handler(event, context):
     try:
         bucket_name = event["detail"]["bucket"]["name"]
         object_key = event["detail"]["object"]["key"]
+        target_lambda_function_name = zip_to_pascal_name(object_key)
 
         lambda_client = boto3.client("lambda")
         lambda_client.update_function_code(
-            FunctionName=os.environ["TARGET_LAMBDA_FUNCTION_NAME"],
+            FunctionName=target_lambda_function_name,
             S3Bucket=bucket_name,
             S3Key=object_key,
         )
@@ -44,3 +44,10 @@ def lambda_handler(event, context):
                 "exception": str(e),
             }
         })
+
+def zip_to_pascal_name(filename: str) -> str:
+    name, _ = os.path.splitext(filename)
+    words: list[str] = name.split("-")[:-1]
+    pascal_case_words = [word.title() for word in words]
+
+    return "".join(pascal_case_words)
