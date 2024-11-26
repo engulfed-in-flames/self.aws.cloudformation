c34dad13d166e899ccdbe42972a3131237085ae5
GyeongSu Gim <recona97@gmail.com>
2024-11-26 11:16:26 +0900

    Update app.py of 'update_function'

--- src/s3/update_function/app.py
+++ src/s3/update_function/app.py
@@ -1,7 +1,10 @@
 import os
 import json
 import boto3
+import botocore
 
+
+region_name = "ap-northeast-1"
 
 def lambda_handler(event, context):
     if not ("httpMethod" in event and event["httpMethod"] == "GET"):
@@ -16,7 +19,12 @@ def lambda_handler(event, context):
         object_key = event["detail"]["object"]["key"]
         target_lambda_function_name = zip_to_pascal_name(object_key)
 
-        lambda_client = boto3.client("lambda")
+        lambda_client = boto3.client("lambda", region_name=region_name)
+
+        # Check the specified lambda function exists or not
+        lambda_client.get_function(FunctionName=target_lambda_function_name)
+
+        # Update the target lambda function
         lambda_client.update_function_code(
             FunctionName=target_lambda_function_name,
             S3Bucket=bucket_name,
@@ -36,6 +44,21 @@ def lambda_handler(event, context):
                 "msg":"The bucket name or object key is not found.",
             }
         })
+    except botocore.exception.ClientError as e:
+        if e.response["Error"]["Code"] == "ResourceNotFoundException":
+            return json.dumps({
+                "statusCode": 400,
+                "body": {
+                    "msg": "There is no lambda function linked to the given object."
+                }
+            })
+        else:
+            return json.dumps({
+                "statusCode": 400,
+                "body": {
+                    "msg": f"{e}"
+                }
+            })
     except Exception as e:
         return json.dumps({
             "statusCode": 500,
